4e62c1c3551a778370829232277c8c0d0e8ff7c6
caarlosdamian <dapecarlos@gmail.com>
2017-07-26 19:58:58 -0500

    Code for primes numbers
    Add de code lines to identify primes numbers

--- primes.py
+++ primes.py
@@ -10,7 +10,17 @@ class PrimeClass(object):
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Metodo para evaluar los numeros primos si cumplen o no lo requerido
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int <2:
+             return False
+        else:            
+        # Cliclo para las diviciones del numero
+            for count in range(2, num_int):
+                #Se evalua el numero
+                if(num_int % count) == 0:
+                    #Si es divicible por otro numero resulta false no primo
+                    return False
+                #Si cumploe es numero primo
+            return True
+       
