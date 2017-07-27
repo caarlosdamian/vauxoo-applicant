6393f1b9f58dd691d2438706b61b267454cfd14f
caarlosdamian <dapecarlos@gmail.com>
2017-07-26 19:08:10 -0500

    Employee
    Tables created : Employee_department and Employee, add information in the rows of the tables.

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,32 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+    departamento_id integer REFERENCES employee_departament,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL
+   
 );
 
 CREATE TABLE employee_department (
+     id serial PRIMARY KEY
+    name varchar(50) NO NULL,
+    description varchar(100) NOT NULL
 );
 
 CREATE TABLE employee_hobby (
 );
+
+INSERT INTO employee (first_name,last_name,departamento_id) VALUES('Carlos','Damian',1);
+INSERT INTO employee (first_name,last_name,departamento_id) VALUES('Marcelo','Sanchez',5);
+INSERT INTO employee (first_name,last_name,departamento_id) VALUES('Javier','Duarte',6);
+INSERT INTO employee (first_name,last_name,departamento_id) VALUES('Enrique','Guzman',2);
+
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Herramientas','Se guardan Herramientas')
+INSERT INTO employee_department (name,description) VALUES ('Departamento de LActeos','Se guardan Lacteo')
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Enlatados','Se guardan Enlatados')
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Contabilidad','Se guardan a los Contadores')
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Computadoras','Se guardan Computadoras')
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Televisiones','Se guardan Televisiones')
+
 
 -- ...
