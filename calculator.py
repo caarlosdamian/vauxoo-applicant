8fdd788f4fb1c0ee768ae39f5a415fe2bc9920a3
caarlosdamian <dapecarlos@gmail.com>
2017-07-26 20:45:28 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,7 +13,7 @@ class CalculatorClass(object):
        Este metodo recibe una entrada de una lista de numeros y devuelve la suma de estos
         """
         #En esta valiable se grardara la suma de los numeros
-        resltudado=0
+        resltudado = 0
         #Cliclo para sumar los numeros de la liosta
         for num in num_list:
             #suma de los numeros
