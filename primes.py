0be871b82fba93f3534b29a384be38c5542eb43e
caarlosdamian <dapecarlos@gmail.com>
2017-07-27 21:51:47 -0500

    fix
    fix

--- primes.py
+++ primes.py
@@ -1,12 +1,12 @@
 class PrimeClass(object):
-        def is_prime (self,num_int):
-                # Cliclo para las diviciones del numero
-                for count in range (2,num_int):
-                        # Si es divicible por otro numero resulta falso no primo
-                        if (num_int % count)==0:
-                                return False
-			# Si Cumple es numero primo
-			return True
+    def is_prime ( self, num_int):
+        # Cliclo para las diviciones del numero
+        for count in range ( 2, num_int):
+                # Si es divicible por otro numero resulta falso no primo
+            if ( num_int % count) == 0:
+                return False
+        # Si Cumple es numero primo
+        return True
 
 
 	
