57b573b0800c65bf75aca869105160e9fab487c4
caarlosdamian <dapecarlos@gmail.com>
2017-07-27 20:37:31 -0500

    Changes
    Changes

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,51 +3,49 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+-- DROP TABLE employee_hobbies;
 -- DROP TABLE employee;
 -- DROP TABLE employee_department;
 -- DROP TABLE employee_hobby;
--- DROP TABLE pasatiempo_hobby;
+
 
 CREATE TABLE employee_department (
-     id serial PRIMARY KEY,
-    name varchar(50) NO NULL,
-    description varchar(100) NOT NULL
+	id serial PRIMARY KEY,
+	name varchar (50) NOT NULL,
+	description varchar (100) NOT NULL
 );
 
 CREATE TABLE employee (
-    id serial PRIMARY KEY,
-    departamento_id integer REFERENCES employee_departament,
-    jefe integer REFERENCES employee,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL
-   
+	id serial PRIMARY KEY,
+	departmento_id integer REFERENCES employee_department,
+	jefe integer REFERENCES employee,
+	first_name varchar (50) NOT NULL,
+	last_name varchar (50) NOT NULL
 );
 
 CREATE TABLE employee_hobby (
-    id serial PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    description varchar(100)NOT NULL
+	id serial PRIMARY KEY,
+	name varchar (50) NOT NULL,
+	description varchar (100) NOT NULL
 );
 
-CREATE TABLE pasatiempo_empleado(
-    employee_id integer REFERENCES employee,
-    hobby_id integer REFERENCES employee_hobby,
-    PRIMARY KEY (employee_id,hobby_id)
+CREATE TABLE pasatiempo_empleado (
+	employee_id integer REFERENCES employee,
+	hobby_id integer REFERENCES employee_hobby,
+	PRIMARY KEY (employee_id, hobby_id)
 );
 
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Herramientas','Se guardan Herramientas');
+INSERT INTO employee_department (name,description) VALUES ('Departamento de LActeos','Se guardan Lacteo');
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Enlatados','Se guardan Enlatados');
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Contabilidad','Se guardan a los Contadores');
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Computadoras','Se guardan Computadoras');
+INSERT INTO employee_department (name,description) VALUES ('Departamento de Televisiones','Se guardan Televisiones');
 
-
-INSERT INTO employee_department (name,description) VALUES ('Departamento de Herramientas','Se guardan Herramientas')
-INSERT INTO employee_department (name,description) VALUES ('Departamento de LActeos','Se guardan Lacteo')
-INSERT INTO employee_department (name,description) VALUES ('Departamento de Enlatados','Se guardan Enlatados')
-INSERT INTO employee_department (name,description) VALUES ('Departamento de Contabilidad','Se guardan a los Contadores')
-INSERT INTO employee_department (name,description) VALUES ('Departamento de Computadoras','Se guardan Computadoras')
-INSERT INTO employee_department (name,description) VALUES ('Departamento de Televisiones','Se guardan Televisiones')
-
-INSERT INTO employee (first_name,last_name,departamento_id,jefe) VALUES('Carlos','Damian',1,4;
-INSERT INTO employee (first_name,last_name,departamento_id,jefe) VALUES('Marcelo','Sanchez',5,4);
-INSERT INTO employee (first_name,last_name,departamento_id,jefe) VALUES('Javier','Duarte',6,2);
-INSERT INTO employee (first_name,last_name,departamento_id,jefe) VALUES('Enrique','Guzman',2,2);
+INSERT INTO employee (first_name, last_name, departmento_id, jefe) VALUES ('Carlos', 'Damian', 2, 1);
+INSERT INTO employee (first_name, last_name, departmento_id, jefe) VALUES ('Manuel', 'sanchez', 4, 1);
+INSERT INTO employee (first_name, last_name, departmento_id, jefe) VALUES ('Javier', 'duarte', 1, 2);
+INSERT INTO employee (first_name, last_name, departmento_id, jefe) VALUES ('Enrique', 'Guzman', 6, 2);
 
 INSERT INTO employee_hobby (name,description) VALUES ('football','Jugar Retas');
 INSERT INTO employee_hobby (name,description) VALUES ('Matematica','Hacer algoritmos');
@@ -61,7 +59,3 @@ INSERT INTO pasatiempo_empleado (employee_id,hobby_id) VALUES (3,2);
 INSERT INTO pasatiempo_empleado (employee_id,hobby_id) VALUES (3,3);
 INSERT INTO pasatiempo_empleado (employee_id,hobby_id) VALUES (4,2);
 INSERT INTO pasatiempo_empleado (employee_id,hobby_id) VALUES (4,1);
-
-
-
--- ...
--- primes.py
+++ primes.py
@@ -8,16 +8,14 @@ class PrimeClass(object):
     Your class documentation here
     """
 
-    def is_prime(num_int):
+    def is_prime(self,num_int):
         """
         Metodo para evaluar si un numero es primo o no
         """
-        if num_int < 2:
-            return False
             # Cliclo para las diviciones del numero
             for count in range(2, num_int):
                 # Se evalua el numero
-                if num_int % count == 0:
+                if (num_int % count)==0:
                     # Si es divicible por otro numero resulta false no primo
                     return False
             #Si cumploe es numero primo
