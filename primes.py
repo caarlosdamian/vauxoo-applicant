4224a2797e573b841a9e4780e177bace7aae6579
caarlosdamian <dapecarlos@gmail.com>
2017-07-27 22:09:52 -0500

    fix taps
    fix taps

--- primes.py
+++ primes.py
@@ -7,16 +7,15 @@ class PrimeClass(object):
     """
     Your class documentation here
     """
-
     def is_prime(self, num_int):
         """
         Metodo para evaluar si un numero es primo o no
         """
-        # Cliclo para las diviciones del numero
-            for count in range(2, num_int):
+        # CLiclo para las diviciones del numero
+        for count in range(2, num_int):
                 # Evaluacion del numero
                 if (num_int % count) == 0:
-                    # Si es divicible por otro numero resulta falso no primo
+                    # Si es divdicible por otro numero resulta falso no primo
                     return False
             # Si Cumple es numero primo
             return True
